223d8d4e0e5f484498ce2e6e5307d9be32ca20ff
Hiroshi Miura <miurahr@linux.com>
2021-11-28 15:23:37 +0900

    Update parser
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/io/github/eb4j/dsl/DslParserTest.java
+++ src/test/java/io/github/eb4j/dsl/DslParserTest.java
@@ -53,16 +53,70 @@ public class DslParserTest {
         assertEquals("[trn][lang name=\"Russian\"]abc[/lang][/trn]", baos.toString());
     }
 
-
     @Test
-    void mean() throws ParseException {
+    void bold() throws ParseException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        DslParser parser = DslParser.createParser("[m][trn][b][c]abc[/c][/b] def [/trn][/m]");
+        DslParser parser = DslParser.createParser("[trn][b][c]abc[/c][/b] def [/trn]");
         DslArticle article = parser.DslArticle();
         DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
         article.accept(dumper);
         dumper.finish();
-        assertEquals("[trn][c color=\"green\"]abc[/c][/trn]", baos.toString());
+        assertEquals("[trn][b][c]abc[/c][/b] def [/trn]", baos.toString());
+    }
+
+    @Test
+    void mean() throws ParseException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DslParser parser = DslParser.createParser("[m]abc[/m]");
+        DslArticle article = parser.DslArticle();
+        DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+        article.accept(dumper);
+        dumper.finish();
+        assertEquals("[m]abc[/m]", baos.toString());
+    }
+
+    @Test
+    void mean1() throws ParseException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DslParser parser = DslParser.createParser("[m1]abc[/m]");
+        DslArticle article = parser.DslArticle();
+        DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+        article.accept(dumper);
+        dumper.finish();
+        assertEquals("[m1]abc[/m]", baos.toString());
+    }
+
+    @Test
+    void unicode() throws ParseException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DslParser parser = DslParser.createParser("[trn]same as 一樣|一样 [t]yī yàng[/t], the same[/trn]");
+        DslArticle article = parser.DslArticle();
+        DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+        article.accept(dumper);
+        dumper.finish();
+        assertEquals("[trn]same as 一樣|一样 [t]yī yàng[/t], the same[/trn]", baos.toString());
+    }
+
+    @Test
+    void complex() throws ParseException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DslParser parser = DslParser.createParser("[m1][b]1.[/b] [trn]отказываться [com]([i]от чего-л.[/i])[/com], прекращать [com]([i]попытки и т. п.[/i])[/com][/trn][/m]");
+        DslArticle article = parser.DslArticle();
+        DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+        article.accept(dumper);
+        dumper.finish();
+        assertEquals("[m1][b]1.[/b] [trn]отказываться [com]([i]от чего-л.[/i])[/com], прекращать [com]([i]попытки и т. п.[/i])[/com][/trn][/m]", baos.toString());
+    }
+
+    @Test
+    void multiline() throws ParseException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DslParser parser = DslParser.createParser("    [m1][b]1.[/b] [trn]отказываться [com]([i]от чего-л.[/i])[/com], прекращать [com]([i]попытки и т. п.[/i])[/com][/trn][/m]\n    [m2]to [ref]abandon attempts[/ref][/m]");
+        DslArticle article = parser.DslArticle();
+        DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+        article.accept(dumper);
+        dumper.finish();
+        assertEquals("    [m1][b]1.[/b] [trn]отказываться [com]([i]от чего-л.[/i])[/com], прекращать [com]([i]попытки и т. п.[/i])[/com][/trn][/m]\n    [m2]to [ref]abandon attempts[/ref][/m]", baos.toString());
     }
 
 }
