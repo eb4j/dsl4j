249dc01f047eafee358afae501ef2f05ee9f11aa
Hiroshi Miura <miurahr@linux.com>
2021-11-28 16:38:11 +0900

    Add test for multi line article
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/test/java/io/github/eb4j/dsl/DslDictionaryTest.java
+++ src/test/java/io/github/eb4j/dsl/DslDictionaryTest.java
@@ -35,4 +35,34 @@ class DslDictionaryTest {
             }
         }
     }
+
+    private static final String EXPECTED_ABANDON =
+            "[m1][b]1.[/b] [trn]отказываться [com]([i]от чего-л.[/i])[/com]," +
+                    " прекращать [com]([i]попытки и т. п.[/i])[/com][/trn][/m]\n" +
+            "[m1][b]2.[/b] [trn]покидать, оставлять[/trn][/m]\n" +
+            "[m2]to [ref]abandon attempts[/ref][/m]\n" +
+            "[m2]to [ref]abandon a claim[/ref][/m]\n" +
+            "[m2]to [ref]abandon convertibility[/ref][/m]\n" +
+            "[m2]to [ref]abandon the \\[gold\\] standard[/ref][/m]\n" +
+            "[m2]to [ref]abandon price control[/ref][/m]\n" +
+            "[m2]to [ref]abandon a right[/ref][/m]\n";
+
+    @Test
+    void loadDicitonaryMulti() throws URISyntaxException, IOException, ParseException {
+        DslDictionary dictionary = DslDictionary.loadDictionary(new File(resource.toURI()));
+        for (Map.Entry<String, Object> entry: dictionary.getEntries("abandon")) {
+            Object val = entry.getValue();
+            if (val instanceof String) {
+                DslParser parser = DslParser.createParser((String) val);
+                DslArticle article = parser.DslArticle();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                DslDumper dumper = new DslDumper(baos, StandardCharsets.UTF_8);
+                article.accept(dumper);
+                dumper.finish();
+                assertEquals(EXPECTED_ABANDON,
+                        baos.toString("UTF-8"));
+                break;
+            }
+        }
+    }
 }
